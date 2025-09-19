51e31b8f6b675622301dc46b03043c5d7ef0741e
yamikhailtolmachev <yamikhailtolmachev@yandex.ru>
2025-09-20 00:06:54 +0400

    Task3. Изменил MyFirstClass и добавил MySecondClass. Задание выполнено

---
+++ Task3/MyFirstProgram.java
@@ -0,0 +1,47 @@
+class MyFirstClass {
+	public static void main(String[] s) {
+		MySecondClass o = new MySecondClass(15, 3);
+		System.out.println(o.division());
+
+		for (int i = 1; i <= 8; i++) {
+ 			for (int j = 1; j <= 8; j++) {
+				o.setp1(i);
+				o.setp2(j);
+				System.out.print(o.division());
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+class MySecondClass {
+	// два приватных поля типа int
+	private int p1, p2;
+	
+	// методы для получения значений полей
+	public int getp1() {
+		return p1;
+	}
+	public int getp2() {
+		return p2;
+	}
+	// методы для модификации значений полей
+	public void setp1(int p1) {
+		this.p1 = p1;
+	}
+	public void setp2(int p2) {
+		this.p2 = p2;
+	}
+	
+	// конструктор, создающий объект и инициализирующий значения полей
+	public MySecondClass(int p1, int p2) {
+		this.p1 = p1;
+		this.p2 = p2;
+	}
+	
+	// метод с возвращаемым типом int, реализующий над этими числами целочисленное деление
+	public int division() {
+		return p1/p2;
+	}
+}
